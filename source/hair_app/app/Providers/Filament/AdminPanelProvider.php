1d30bf27e2ad7dfcf65e54ca5989ab49be119517
nsilvera <nsilvera@lapampa.gob.ar>
2024-09-05 00:17:30 -0300

    optimizacion de filament

--- source/hair_app/app/Providers/Filament/AdminPanelProvider.php
+++ source/hair_app/app/Providers/Filament/AdminPanelProvider.php
@@ -23,7 +23,6 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->default()
             ->id('admin')
             ->path('admin')
             ->login()
