8b9aed3b90c73c8f36bb9f88192225eb90c97af7
nsilvera <nsilvera@lapampa.gob.ar>
2024-12-06 22:21:45 -0300

    Reemplazo K-OPT por OR TOOLS

--- source/hair_service/hair/procedures.py
+++ source/hair_service/hair/procedures.py
@@ -6,8 +6,9 @@ from typing import Type
 
 from hair.mip1 import Mip1
 from hair.mip2 import Mip2
-from tsp_local.base import TSP
-from tsp_local.kopt import KOpt
+
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
 
 from modelos.tabulists import tabulists
 
@@ -82,13 +83,13 @@ def movimiento(solucion: Type["Solucion"], iterador_principal: int) -> Type["Sol
     return respuesta
 
 
-def mejora(solucion_original : Type["Solucion"], iterador_principal : int) -> Type["Solucion"]:
+def mejora(solucion : Type["Solucion"], iterador_principal : int) -> Type["Solucion"]:
     """
     Aplica un proceso iterativo de mejora a la solución dada utilizando tres tipos diferentes de optimizaciones:
     
-    1. Aplicación del modelo MIP1 seguido del algoritmo Lin-Kernighan (LK).
+    1. Aplicación del modelo MIP1 seguido del tsp_solver.
     2. Fusión de pares consecutivos de rutas y aplicación del modelo MIP2 con ajustes para asegurar factibilidad.
-    3. Aplicación directa del modelo MIP2 seguido del algoritmo LK.
+    3. Aplicación directa del modelo MIP2 seguido del algoritmo tsp_solver.
     
     Durante cada iteración, si alguna de las soluciones generadas mejora el costo de la solución actual, se actualiza
     dicha solución y se continúa el proceso.
@@ -102,21 +103,20 @@ def mejora(solucion_original : Type["Solucion"], iterador_principal : int) -> Ty
     """  
     do_continue = True
     
-    solucion = solucion_original.clonar()
-    mejor_solucion = lk(Solucion.obtener_empty_solucion(), solucion)
+    mejor_solucion = tsp_solver(Solucion.obtener_empty_solucion(), solucion)
 
     while do_continue:
         do_continue = False
 
         #################### PRIMER TIPO DE MEJORA ##################
-        #Se aplica el MIP1 a mejor_solucion, luego se le aplica LK
+        #Se aplica el MIP1 a mejor_solucion, luego se le aplica tsp_solver
         solucion_prima = Mip1.ejecutar(mejor_solucion)
-        solucion_prima = lk(mejor_solucion, solucion_prima)
+        solucion_prima = tsp_solver(mejor_solucion, solucion_prima)
         #Si el costo de la solución encontrada es mejor que el de mejor_solucion, se actualiza mejor_solucion
         if (solucion_prima.costo < mejor_solucion.costo):
             mejor_solucion = solucion_prima.clonar()
             do_continue = True
-            # print(f"Mejora 1 {mejor_solucion}")
+            print(f"Mejora 1 {mejor_solucion}")
         
         #################### SEGUNDO TIPO DE MEJORA ####################
         solucion_merge = mejor_solucion.clonar()
@@ -136,7 +136,7 @@ def mejora(solucion_original : Type["Solucion"], iterador_principal : int) -> Ty
                 
             #Si el resultado de aplicar el MIP2 a s1 es factible, entonces solucion_prima es una solución óptima
             if aux_solucion.es_factible:
-                solucion_prima = lk(s1, aux_solucion)
+                solucion_prima = tsp_solver(s1, aux_solucion)
                 if solucion_prima.costo < solucion_merge.costo:
                     solucion_merge = solucion_prima.clonar()
 
@@ -153,7 +153,7 @@ def mejora(solucion_original : Type["Solucion"], iterador_principal : int) -> Ty
                 
             #Si el resultado de aplicar el MIP2 a s2 es factible, entonces solucion_prima es una solución óptima
             if aux_solucion.es_factible:
-                solucion_prima = lk(s2, aux_solucion)
+                solucion_prima = tsp_solver(s2, aux_solucion)
                 #En este punto solucion_merge es la mejor solución entre mejor_solucion y la primer parte de esta mejora.
                 if solucion_prima.costo < solucion_merge.costo:
                     solucion_merge = solucion_prima.clonar()
@@ -165,20 +165,19 @@ def mejora(solucion_original : Type["Solucion"], iterador_principal : int) -> Ty
             # print(f"Mejora 2 {mejor_solucion}")
 
         #TERCER TIPO DE MEJORA
-        #Se aplica el MIP2 a mejor_solucion, luego se le aplica LK
+        #Se aplica el MIP2 a mejor_solucion, luego se le aplica tsp_solver
         solucion_prima = Mip2.ejecutar(mejor_solucion)
-        solucion_prima = lk(mejor_solucion, solucion_prima)
+        solucion_prima = tsp_solver(mejor_solucion, solucion_prima)
         if solucion_prima.costo < mejor_solucion.costo:
             mejor_solucion = solucion_prima.clonar()
             do_continue = True 
             print(f"Mejora 3 {mejor_solucion}")
 
-    print(f"Mejora ({iterador_principal}): {solucion_prima}")
+    print(f"Mejora ({iterador_principal}): {mejor_solucion}")
     return mejor_solucion.clonar()
 
 def salto(solucion, iterador_principal):
     mejor_solucion = solucion.clonar()
-    print(len(triplet_manager.triplets))
     if len(triplet_manager.triplets) > 0:
         #Mientras haya triplets
         while triplet_manager.triplets:
@@ -200,37 +199,92 @@ def salto(solucion, iterador_principal):
     print(f"Salto ({iterador_principal}): {mejor_solucion}")
     return mejor_solucion.clonar()
 
-def lk(solucion: Type["Solucion"], solucion_prima: Type["Solucion"]) -> Type["Solucion"]:
-    if solucion.es_igual(solucion_prima):
-        aux_solucion = solucion.clonar()
-    else:
-        aux_solucion = solucion_prima.clonar()
-        for tiempo in range(constantes.horizonte_tiempo):
-            tamano_matriz = len(solucion_prima.rutas[tiempo].clientes)+1
-            matriz =  [[0] * tamano_matriz for _ in range(tamano_matriz)]
+def tsp_solver(solucion: Type["Solucion"], solucion_prima: Type["Solucion"]) -> Type["Solucion"]:
+    """
+    Función resuelve un TSP para mejorar las rutas de una solución.
+    
+    Args:
+        solucion (Type["Solucion"]): Solución inicial.
+        solucion_prima (Type["Solucion"]): Solución alternativa.
 
-            # Proveedor distancia
-            for indice, cliente in enumerate(solucion_prima.rutas[tiempo].clientes):
-                matriz[0][indice+1] = cliente.distancia_proveedor
-                matriz[indice+1][0] = cliente.distancia_proveedor
+    Returns:
+        Type["Solucion"]: Solución mejorada después de aplicar el algoritmo.
+    """
+    # Determinar la solución base a clonar
+    aux_solucion = solucion.clonar() if solucion.es_igual(solucion_prima) else solucion_prima.clonar()
 
-            # Clientes distancias
-            for indice, c in enumerate(solucion_prima.rutas[tiempo].clientes):
-                for indice2, c2 in enumerate(solucion_prima.rutas[tiempo].clientes):
-                    matriz[indice+1][indice2+1] = constantes.matriz_distancia[c.id][c2.id]
+    # Iterar sobre el horizonte de tiempo
+    for t in range(constantes.horizonte_tiempo):
+        
+        # Obtener matriz distancia
+        matriz_distancia = _obtener_matriz_distancia(aux_solucion.rutas[t])
 
-            # Make an instance with all nodes
-            TSP.setEdges(matriz)
-            path, costo = KOpt(range(len(matriz))).optimise()
-            
-            aux_solucion.rutas[tiempo] = Ruta(
-                [solucion_prima.rutas[tiempo].clientes[indice - 1] for indice in path[1:]],
-                [solucion_prima.rutas[tiempo].cantidades[indice - 1] for indice in path[1:]]
+        # Configurar los datos del problema
+        data = {
+            "distance_matrix": matriz_distancia,  # Matriz de distancias
+            "num_vehicles": 1,                    # Un solo vehículo
+            "depot": 0                            # Nodo inicial (proveedor)
+        }
+
+        # Crear el gestor de índices de ruteo
+        manager = pywrapcp.RoutingIndexManager(
+            len(data["distance_matrix"]), data["num_vehicles"], data["depot"]
+        )
+
+        # Crear el modelo de ruteo
+        routing = pywrapcp.RoutingModel(manager)
+
+        transit_callback_index = routing.RegisterTransitCallback(
+            lambda from_index, to_index: data["distance_matrix"][
+                manager.IndexToNode(from_index)
+            ][manager.IndexToNode(to_index)]
+        )
+
+        # Establecer el costo de cada arco
+        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+        # Configurar los parámetros de búsqueda
+        parametros_busqueda = pywrapcp.DefaultRoutingSearchParameters()
+        parametros_busqueda.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+        # Resolver el problema
+        solucion_modelo = routing.SolveWithParameters(parametros_busqueda)
+
+        # Si se encuentra una solución válida, actualizar la ruta en la solución auxiliar
+        if solucion_modelo:
+            ruta_optima = []  # Almacena la ruta óptima
+            index = routing.Start(0)  # Nodo inicial (proveedor)
+            # Recorrer la solución y construir la ruta óptima
+            while not routing.IsEnd(index):
+                siguiente_index = solucion_modelo.Value(routing.NextVar(index))
+                if manager.IndexToNode(siguiente_index) != 0:  # Excluir el nodo inicial
+                    ruta_optima.append(manager.IndexToNode(siguiente_index))
+                index = siguiente_index
+
+            # Crear una nueva ruta con los clientes y cantidades de la solución auxiliar
+            nueva_ruta = Ruta(
+                [aux_solucion.rutas[t].clientes[indice - 1] for indice in ruta_optima],
+                [aux_solucion.rutas[t].cantidades[indice - 1] for indice in ruta_optima]
             )
+            aux_solucion.rutas[t] = nueva_ruta.clonar()
 
+    # Actualizar y retornar la solucion optimizada
     aux_solucion.refrescar()
     return aux_solucion
 
+def _obtener_matriz_distancia(ruta):
+    # Crear la matriz de distancias entre los clientes visitados en el tiempo t
+    matriz_distancia = [[0] + [c.distancia_proveedor for c in ruta.clientes]]
+
+    for cliente1 in ruta.clientes:
+        fila = [cliente1.distancia_proveedor]
+        fila.extend(
+            constantes.matriz_distancia[cliente1.id][cliente2.id]
+            for cliente2 in ruta.clientes
+        )
+        matriz_distancia.append(fila)
+    return matriz_distancia
+    
 # Satélites de MOVER
 def _crear_n_prima(solucion: Type["Solucion"]) -> list[Type["Solucion"]]:
     """
@@ -242,14 +296,7 @@ def _crear_n_prima(solucion: Type["Solucion"]) -> list[Type["Solucion"]]:
     Returns:
         list[Solucion]: Lista de soluciones generadas por las variaciones.
     """
-    # with concurrent.futures.ThreadPoolExecutor() as executor:
-        # # Ejecutar las variantes en hilos separados
-        # futuro_eliminacion   = executor.submit(_variante_eliminacion, solucion)
-        # futuro_insercion     = executor.submit(_variante_insercion, solucion)
-        # futuro_mover_visita  = executor.submit(_variante_mover_visita, solucion)
-        # futuro_intercambiar  = executor.submit(_variante_intercambiar_visitas, solucion)
-
-        # Esperar a que todas las tareas finalicen y recolectar los resultados
+    # Esperar a que todas las tareas finalicen y recolectar los resultados
     vecindario_prima = []
     vecindario_prima += _variante_eliminacion(solucion)
     vecindario_prima += _variante_insercion(solucion)
